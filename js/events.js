f46ada99761a4a4444a5be01f5e40d367daa2185
Vedant Chaudhari <c.vedant18@gmail.com>
2022-03-09 14:51:13 +0530

    Add event page

--- js/events.js
+++ js/events.js
@@ -2,66 +2,68 @@
 
 const events = [
   {
+    id:1,
     title: "Perplexo",
     img: "./assets/icon1.png",
     prize: "Rs. 5000",
   },
   {
+    id:2,
     title: "It's Debatable",
     img: "./../assets/icon1.png",
     prize: "Rs. 5000",
+    link:"/events/perplexo",
   },
   {
+    id:3,
     title: "Design & Develop",
     img: "./../assets/icon1.png",
     prize: "Rs. 5000",
+    link:"/events/designanddevelop",
   },
   {
+    id:4,
     title: "Perplexo",
     img: "./assets/icon1.png",
     prize: "Rs. 5000",
+    link:"/events/xyz",
   },
   {
+    id:5,
     title: "It's Debatable",
     img: "./../assets/icon1.png",
     prize: "Rs. 5000",
-  },
-  {
-    title: "Design & Develop",
-    img: "./../assets/icon1.png",
-    prize: "Rs. 5000",
-  },
-  {
-    title: "Perplexo",
-    img: "./assets/icon1.png",
-    prize: "Rs. 5000",
-  },
-  {
-    title: "It's Debatable",
-    img: "./../assets/icon1.png",
-    prize: "Rs. 5000",
-  },
-  {
-    title: "Design & Develop",
-    img: "./../assets/icon1.png",
-    prize: "Rs. 5000",
+    link:"/events/abc",
   },
 ];
 
-const container = document.querySelector(".events-container");
 
-function returnCards(events) {
-  return events.map(
-    (event) => `
+// load and display cards on main page
+
+const routeChange=(e)=>{
+  // console.log(e.dataset)
+  location.href = "/events/#"+e.dataset.path
+}
+
+
+const load = async ()=>{
+  let res =  await fetch("https://raw.githubusercontent.com/pccoeacm/data-store/main/events.json");
+  let data = await res.json()
+
+  $("#events-container").html("")
+
+  data.forEach(
+    (event) => {
+    $('.events-container').append(`
         <div class="card event-card m-3" style="width:23rem">
         <div class="card-body">
               <div class="d-flex align-items-center card-head">
-                <img src="${event.img}" alt="logo" class="event-card-icon"/>
-                <h5 class="event-title">${event.title}</h5>
+                <img src="" alt="logo" class="event-card-icon"/>
+                <h5 class="event-title">${event.eventName}</h5>
               </div>
               <div class="prizes m-2">
                 <h4>
-                  ${event.prize}
+                  ${event.cellName}
                 </h4>
               </div>
 
@@ -69,11 +71,15 @@ function returnCards(events) {
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
               
-              <button class="event-card-button">Register!</button>
+              <button class="event-card-button" data-path=${event.eventName.split(" ").join("_")} onclick="routeChange(this)">See Details</button>
            
-        </div>
-        </div>`
-  );
+          </div>
+        </div>`)
+    }
+    );
+    
 }
 
-container.innerHTML = returnCards(events);
+
+
+  load()
